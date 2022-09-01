b3c0b5c30942c91ec848935b7b00caf710175fd2
SeolYoungKim <nasur4da@gmail.com>
2022-09-01 21:15:54 +0900

    Greedy

---
+++ CodingTest/src/ThisIsCodingTest/Greedy.java
@@ -0,0 +1,30 @@
+package ThisIsCodingTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Greedy {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String readLine = br.readLine();
+        String[] strings = readLine.split(" ");
+
+        int n = Integer.parseInt(strings[0]);
+        int k = Integer.parseInt(strings[1]);
+
+        int count = 0;
+
+        while (n != 1) {
+            if (n % k == 0) {
+                count += 1;
+                n = n / k;
+            } else {
+                n -= 1;
+                count += 1;
+            }
+        }
+
+        System.out.println("after count = " + count);
+    }
+}
