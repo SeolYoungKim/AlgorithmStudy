2e3f93aadad4a6505df926d628fbd16708f7a59a
SeolYoungKim <nasur4da@gmail.com>
2022-09-05 00:39:42 +0900

    백준 함수까지 완료

---
+++ CodingTest/src/baekjoon2/Bj4673.java
@@ -0,0 +1,31 @@
+package baekjoon2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Bj4673 {
+    public static void main(String[] args) {
+        List<String> numbers = IntStream.range(1, 10001)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.toList());
+
+        ArrayList<String> forRemove = new ArrayList<>();
+
+        for (String number : numbers) {
+            Integer remove = Integer.parseInt(number);
+
+            for (int i = 0; i < number.length(); i++) {
+                remove += Integer.parseInt(String.valueOf(number.charAt(i)));
+            }
+            if (remove < 10001) forRemove.add(String.valueOf(remove));
+        }
+
+        numbers.removeAll(forRemove);
+
+        for (String number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
