e9c5fa3a25af6d5c9342d2d553b981fd9f2d2377
SeolYoungKim <nasur4da@gmail.com>
2022-09-09 22:12:09 +0900

    [LeetCode] HashMap-easy 22.09.09

---
+++ CodingTest/src/LeetCode/hashtable/L1122.java
@@ -0,0 +1,58 @@
+package LeetCode.hashtable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class L1122 {
+    public static void main(String[] args) {
+        int[] arr1 = {2,21,43,38,0,42,33,7,24,13,12,27,12,24,5,23,29,48,30,31}, arr2 = {2,42,38,0,43,21};
+
+        System.out.println(Arrays.toString(method(arr1, arr2)));
+    }
+
+    public static int[] method(int[] arr1, int[] arr2) {
+        Arrays.sort(arr1);
+
+        ArrayList<Integer> inArr2 = new ArrayList<>();
+        ArrayList<Integer> notInArr2 = new ArrayList<>();
+
+        for (int num1 : arr1) {
+            if (Arrays.stream(arr2).noneMatch(i -> i == num1)) {
+                notInArr2.add(num1);
+            }
+        }
+
+        for (int num2 : arr2) {
+            for (int num1 : arr1) {
+                if (num2 == num1) {
+                    inArr2.add(num1);
+                }
+            }
+        }
+
+        inArr2.addAll(notInArr2);
+
+        for (int i = 0; i < inArr2.size(); i++) {
+            arr1[i] = inArr2.get(i);
+        }
+
+        return arr1;
+
+        // 이렇게도 푼다
+//        int[] cnt = new int[1001];
+//        for(int n : arr1) cnt[n]++;
+//        int i = 0;
+//        for(int n : arr2) {
+//            while(cnt[n]-- > 0) {
+//                arr1[i++] = n;
+//            }
+//        }
+//        for(int n = 0; n < cnt.length; n++) {
+//            while(cnt[n]-- > 0) {
+//                arr1[i++] = n;
+//            }
+//        }
+//        return arr1;
+
+    }
+}
---
+++ CodingTest/src/LeetCode/hashtable/L1160.java
@@ -0,0 +1,8 @@
+package LeetCode.hashtable;
+
+/**
+ * https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/
+ * 여기서부터
+ */
+public class L1160 {
+}
