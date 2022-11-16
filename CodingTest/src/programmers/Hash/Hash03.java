8a6ee8c9ba7b9e944690dab9186cdefd127519f1
SeolYoungKim <nasur4da@gmail.com>
2022-11-16 13:40:39 +0900

    hash03

--- CodingTest/src/programmers/Hash/Hash03.java
+++ CodingTest/src/programmers/Hash/Hash03.java
@@ -1,16 +1,62 @@
 package programmers.Hash;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.HashMap;
+import org.junit.platform.commons.util.StringUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Hash03 {
+
+    public static void main(String[] args) {
+        Hash03 hash03 = new Hash03();
+        String[] strArr = {"12", "123", "1235", "567", "88"};
+        boolean solution = hash03.solution(strArr);
+
+        System.out.println(solution);
+    }
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
+        HashMap<String, Integer> map = new HashMap<>();
 
+        for (String s : phone_book) {
+            map.put(s, 0);
+        }
 
-        return answer;
+        for (String phoneNum : phone_book) {
+            for (int i = 1; i < phoneNum.length(); i++) {
+                if (map.containsKey(phoneNum.substring(0, i))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
+    private static class PhoneNumber {
+        private final String number;
 
+        public PhoneNumber(String number) {
+            this.number = number;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            PhoneNumber that = (PhoneNumber) o;
+            return number.startsWith(that.number) || that.number.startsWith(number);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(1);
+        }
+
+        @Override
+        public String toString() {
+            return "PhoneNumber{" +
+                    "number='" + number + '\'' +
+                    '}';
+        }
+    }
 }
