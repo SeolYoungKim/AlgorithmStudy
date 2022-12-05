dd6e4e01ee791037cc1600f32cd2cda4c9dd7644
SeolYoungKim <nasur4da@gmail.com>
2022-12-05 12:07:25 +0900

    [BaekJoon] Queue 문제 풀이

---
+++ baekjoon-study/src/main/java/chapter_01/data_structure/_04_stack_and_queue/_03_Queue.java
@@ -0,0 +1,30 @@
+package chapter_01.data_structure._04_stack_and_queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class _03_Queue {
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(br.readLine());
+            Queue<Integer> queue = new ArrayDeque<>();
+            for (int i = 1; i <= n; i++) {
+                queue.offer(i);
+            }
+
+            int order = n;
+            while (queue.size() > 1) {
+                //버리고 -> 옮기고 를 순차적으로 어떻게 할것이냐
+                queue.poll();
+                Integer poll = queue.poll();
+                queue.offer(poll);
+            }
+
+            System.out.println(queue.poll());
+        }
+    }
+}
