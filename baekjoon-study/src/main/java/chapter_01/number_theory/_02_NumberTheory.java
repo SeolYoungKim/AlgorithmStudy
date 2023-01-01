19aa0eef77c9d93a1724e5c0681ab5a45036f69d
SeolYoungKim <nasur4da@gmail.com>
2023-01-01 14:24:53 +0900

    [BaekJoon] 정수론 문제 풀이

---
+++ baekjoon-study/src/main/java/chapter_01/number_theory/_02_NumberTheory.java
@@ -0,0 +1,54 @@
+package chapter_01.number_theory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 백준 1456번 "거의 소수 구하기"
+ */
+public class _02_NumberTheory {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+
+            int maxNum = 10000000;
+            long[] numbers = new long[maxNum + 1];
+            for (int i = 0; i < maxNum; i++) {
+                numbers[i] = i;
+            }
+
+            for (int i = 2; i <= Math.sqrt(maxNum); i++) {
+                if (numbers[i] == 0) {
+                    continue;
+                }
+
+                for (int j = i * i; j < numbers.length; j += i) {
+                    numbers[j] = 0;
+                }
+            }
+
+            int count = 0;
+            for (int i = 2; i <= maxNum; i++) {
+                if (numbers[i] == 0) {
+                    continue;
+                }
+
+                long temp = numbers[i];
+                while ((double) numbers[i] <= (double) b / temp) {
+                    if ((double) numbers[i] >= (double) a / temp) {
+                        count++;
+                    }
+
+                    temp *= numbers[i];
+                }
+            }
+
+            System.out.println(count);
+
+        }
+    }
+}
