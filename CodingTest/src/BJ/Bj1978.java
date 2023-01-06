093cd06f07052449b40cd4df32f21d53adce8f3a
SeolYoungKim <nasur4da@gmail.com>
2023-01-06 13:42:09 +0900

    [BaekJoon] 클래스 레벨 2 문제 풀이

---
+++ CodingTest/src/BJ/Bj1978.java
@@ -0,0 +1,33 @@
+package BJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Bj1978 {
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int n = Integer.parseInt(br.readLine());
+
+            // 1000개 까지의 소수를 구한다
+            boolean[] isNotPrime = new boolean[1001];
+            isNotPrime[0] = isNotPrime[1] = true;
+            for (int i = 2; i <= Math.sqrt(1000); i++) {
+                for (int j = i * i; j < isNotPrime.length; j += i) {
+                    isNotPrime[j] = true;
+                }
+            }
+
+            int count = 0;
+            String[] numbers = br.readLine().split(" ");
+            for (String number : numbers) {
+                int intNum = Integer.parseInt(number);
+                if (!isNotPrime[intNum]) {
+                    count++;
+                }
+            }
+
+            System.out.println(count);
+        }
+    }
+}
