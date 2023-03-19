ebefad8da6d2e62c53ebf81454a8053d83a58496
SeolYoungKim <nasur4da@gmail.com>
2023-03-19 10:44:00 +0900

    feat: Daily-leetcode setting

---
+++ daily-leetcode/src/main/java/org/daily/_2023/_03/_19.java
@@ -0,0 +1,18 @@
+package org.daily._2023._03;
+
+class _19 {
+    static class WordDictionary {
+
+        public WordDictionary() {
+
+        }
+
+        public void addWord(String word) {
+
+        }
+
+        public boolean search(String word) {
+
+        }
+    }
+}
