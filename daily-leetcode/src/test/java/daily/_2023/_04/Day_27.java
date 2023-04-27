842a9cd957cb3fa0d5c39a9d752b304e5565043f
김설영 <kimsy8979@mrblue.com>
2023-04-27 10:20:38 +0900

    [LeetCode] 2023/04/27 문제 풀이

--- daily-leetcode/src/test/java/daily/_2023/_04/Day_27.java
+++ daily-leetcode/src/test/java/daily/_2023/_04/Day_27.java
@@ -2,34 +2,41 @@ package daily._2023._04;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Day_27 {
-    public int addDigits(int num) {
-        if (num == 0) {
-            return 0;
+    public int bulbSwitchMyVer(int n) {  // 시간 초과남ㅋㅋㅋㅋㅋㅋㅋ
+        final boolean[] bulbs = new boolean[n];
+        // round 1
+        Arrays.fill(bulbs, true);
+
+        // round 2~
+        for (int round = 2; round <= n; round++) {  //round2 ~ roundN
+            for (int idx = (round - 1); idx < n; idx += round) {
+                bulbs[idx] = !bulbs[idx];
+            }
         }
 
-        final int digitCount = (int) (Math.log10(num) + 1);
-        int divider = 10;
-        int result = 0;
-        for (int i = 0; i < digitCount; i++) {
-            result += (num % divider);
-            num /= divider;
-            divider *= 10;
+        int count = 0;
+        for (boolean bulb : bulbs) {
+            if (bulb) {
+                count++;
+            }
         }
 
-        if (digitCount == 1) {
-            return result;
-        }
+        return count;
+    }
 
-        return addDigits(result);
+    public int bulbSwitch(int n) {
+        return (int) Math.sqrt(n);
     }
 
     @Test
     void test() {
-//        assertThat(addDigits(38)).isEqualTo(2);
-        assertThat(addDigits(0)).isEqualTo(0);
+        assertThat(bulbSwitch(3)).isEqualTo(1);
+        assertThat(bulbSwitch(0)).isEqualTo(0);
+        assertThat(bulbSwitch(1)).isEqualTo(1);
     }
 }
