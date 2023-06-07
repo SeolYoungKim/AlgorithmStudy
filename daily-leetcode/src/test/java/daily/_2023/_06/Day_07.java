ea92d07e8f926b4ed59a65b42d8a6db6955542ae
SeolYoungKim <nasur4da@gmail.com>
2023-06-07 22:28:50 +0900

    [LeetCode] 2023/06/07 문제 풀이 - 설명 추가

--- daily-leetcode/src/test/java/daily/_2023/_06/Day_07.java
+++ daily-leetcode/src/test/java/daily/_2023/_06/Day_07.java
@@ -8,19 +8,19 @@ public class Day_07 {
     public int minFlips(int a, int b, int c) {
         int flips = 0;
         while (a > 0 || b > 0 || c > 0) {
-            int bitA = a & 1;
+            int bitA = a & 1;  // 최하위 비트를 비트 & 연산을 통해 얻을 수 있음
             int bitB = b & 1;
             int bitC = c & 1;
 
             if (bitC == 0) {
-                flips += (bitA + bitB);
-            } else {
-                if (bitA == 0 && bitB == 0) {
-                    flips += 1;
+                flips += (bitA + bitB);  // 1인 비트를 뒤집어야 함 (bitA=1, bitB=0이면 1개만 뒤집고, 둘다 1이면 2개를 뒤집음)
+            } else {  // bitC가 1인 경우
+                if (bitA == 0 && bitB == 0) {  // 둘 다 0일때, 한 개만 뒤집으면 됨
+                    flips += 1;  // 둘 다 1인 경우는 더해서 0이되므로 안셈
                 }
             }
 
-            a >>= 1;
+            a >>= 1;  // 1비트씩 오른쪽으로 시프트하여 다음 최하위 비트를 구함
             b >>= 1;
             c >>= 1;
         }
